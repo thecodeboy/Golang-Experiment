f71ebe9cf9093307bd6e4d04f68d437a770de259
“Aishik <aishik.me@gmail.com>
2019-11-24 18:57:30 +0530

    Added inner package 'client'

---
+++ client/client.go
@@ -0,0 +1,12 @@
+/*
+	This program is to test the importing of
+	packages inside directories in the main repo.
+*/
+
+package client
+
+import "fmt"
+
+func hello() {
+	fmt.Println("Hello from the library!")
+}
